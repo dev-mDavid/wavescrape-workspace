5ac5ec345e82213d21507b10fa2a58537afd0f3c
mDavid <dev.mdavid@gmail.com>
2020-05-20 21:19:29 -0700

    create feature module for material library for easier and more robust testing

--- apps/wavescrape/src/app/admin-page/admin-page.module.ts
+++ apps/wavescrape/src/app/admin-page/admin-page.module.ts
@@ -1,26 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
- 
+
 import { AdminPageRoutingModule } from './admin-page-routing.module';
+import { AdminFeatureModule } from './admin-feature/admin-feature.module';
+
 import { AdminPageComponent } from './admin-page.component';
 import { AdminShellComponent } from './admin-shell/admin-shell.component';
 import { AdminBreaksPageComponent } from './admin-page-pages/admin-breaks-page/admin-breaks-page.component';
 import { AddBreakDialogComponent } from './admin-dialogs/add-break-dialog/add-break-dialog.component';
-import { SharedModule } from '../shared/shared.module';
-import { MatDialogModule } from '@angular/material/dialog';
-// import { MatFormField } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon'; 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { AdminFeatureModule } from './admin-feature/admin-feature.module';
+
 
 @NgModule({
   declarations: [
-    AdminShellComponent,
     AdminPageComponent,
+    AdminShellComponent,
     AdminBreaksPageComponent,
     AddBreakDialogComponent
   ],
