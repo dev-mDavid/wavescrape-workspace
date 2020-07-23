fb002ed03a08e7a836512188580bc87d9ce9c916
mDavid <dev.mdavid@gmail.com>
2020-07-23 12:19:36 -0700

    delete old src folder and rename server folder to src

---
+++ src/test-module/test-scrape/test-scrape.controller.ts
@@ -0,0 +1,8 @@
+console.log("hello universe")
+
+// import { Controller } from '@nestjs/common';
+
+// @Controller('test-scrape')
+// export class TestScrapeController {
+//   console.log('hello world')
+// }
