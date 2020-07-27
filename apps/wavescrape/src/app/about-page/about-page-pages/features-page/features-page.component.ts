c13cdb5b5f962dc861bc2d759ea31d84d7faf27a
mDavid <dev.mdavid@gmail.com>
2020-07-26 21:14:51 -0700

    start styling features page

--- apps/wavescrape/src/app/about-page/about-page-pages/features-page/features-page.component.ts
+++ apps/wavescrape/src/app/about-page/about-page-pages/features-page/features-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Breakpoints } from "@angular/cdk/layout";
+import { DeviceResponsiveService } from "../../../core/device-responsive.service";
 
 @Component({
   selector: 'wavescrape-workspace-features-page',
@@ -6,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./features-page.component.sass']
 })
 export class FeaturesPageComponent implements OnInit {
+  isHandset$: any;
+  isTablet$: any;
+  isWeb$: any;
 
-  constructor() { }
+  constructor(
+    DRS: DeviceResponsiveService
+  ) {
+    this.isHandset$ = DRS.whatDevice([Breakpoints.XSmall]);
+    this.isTablet$ = DRS.whatDevice([Breakpoints.Small]);
+    this.isWeb$ = DRS.whatDevice([Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge]);
+  }
+
+  scrollToDiv(target): void {
+    target.scrollIntoView({ 
+      behavior: "smooth", 
+      block: "start", 
+      inline: "nearest" 
+    });
+  }  
 
   ngOnInit(): void {
   }
